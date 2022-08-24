ae3732047602a46a4fe2c92dea29436bef0f20ef
v0idhack <johnosaretin7878@gmail.com>
2022-08-24 08:32:57 +0100

    where users are redirected to after login

---
+++ src/routes/welcome/+page.svelte
@@ -0,0 +1,9 @@
+<div class="h-screen flex justify-center align-middle place-items-center w-full">
+	<p class="max-w-md w-full">
+		<span class="text-green-600 text-lg">congratulations!!</span>
+		<br />
+		your account has be created successfully, check your email for a confirmation link, if you don't
+		see the message on your inbox, it possible it has been flagged by your email provider, so be sure
+		to check your spam folder
+	</p>
+</div>
