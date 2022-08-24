615c93934c20550f2b62f581adc47374ce03be88
v0idhack <johnosaretin7878@gmail.com>
2022-08-24 08:30:38 +0100

    site home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div class="h-[60vh] bg-gray-700 isolate relative overflow-hidden">
+	<img
+		src="https://source.unsplash.com/3000x800?market"
+		class="h-full w-full object-cover"
+		alt=""
+	/>
+	<div
+		class="z-20 absolute inset-0 flex justify-center align-middle place-items-center backdrop-blur-sm bg-slate-900 bg-opacity-50"
+	>
+		<div class="flex flex-col gap-4">
+			<h1 class="text-white text-4xl border-b-2 pb-4 w-fit pr-6">Bring back trust!</h1>
+			<p class="text-gray-100">
+				Our mission is to restore the trust that was stolen from you, so you can expirience the joy
+				of raw talent
+			</p>
+
+			<div class="flex gap-4 mt-4">
+				<a href="/about" class="border-2 border-white px-8 py-2 text-white rounded-md relative"
+					>learn more</a
+				>
+				<a href="/login" sveltekit:prefetch class="bg-orange-500 px-8 py-2 text-white rounded-md relative"
+					>get started</a
+				>
+			</div>
+		</div>
+	</div>
+</div>
